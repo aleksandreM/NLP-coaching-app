1f2f8b1664c33e02040100585d9039dcda215791
aleksandre murghulia <alexandre.murgulia@gmail.com>
2023-04-14 23:16:18 +0400

    ongonig work on sign in page

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -17,27 +17,27 @@ class SignInScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Image.asset(
-                  'assets/images/mandalasemfundo-01.png',
-                  height: 250,
-                  width: 250,
-                ),
+              'assets/images/mandalasemfundo-01.png',
+              height: 250,
+              width: 250,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: const [
                 Text(
                   ' Hello',
                 ),
-                SizedBox(width: 15,),
+                SizedBox(
+                  width: 15,
+                ),
                 Text(' __place holder for name__!'),
               ],
             ),
-          const Text('How do you feel today?'),
+            const Text('How do you feel today?'),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                CustomIconButton(
-                  icon: Icons.sentiment_very_satisfied
-                ),
+                CustomIconButton(icon: Icons.sentiment_very_satisfied),
                 CustomIconButton(
                   icon: Icons.sentiment_satisfied,
                 ),
@@ -52,40 +52,77 @@ class SignInScreen extends StatelessWidget {
                 ),
               ],
             ),
-            Container(
-              
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color:Colors.white),
-              
-              margin: const EdgeInsets.only(left: 20, right: 20),
-              padding: const EdgeInsets.all(50),
-              
-              child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children:const [
-                  
-                  Text('data'),
-                  Text('second data'),
-                ],
-              ),
+            const CustomCircularContainer(
+              aim: 'Objectives',
+              imagePath: 'assets/images/objectivos.jpeg',
+              secondAim: 'to do',
+              secondImagePath: 'assets/images/Tohappen.jpg',
             ),
-            Container(
-              
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color:Colors.white),
-              
-              margin: const EdgeInsets.only(left: 20, right: 20),
-              padding: const EdgeInsets.all(50),
-              
-              child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children:const [
-                  
-                  Text('data'),
-                  Text('second data'),
-                ],
-              ),
-            )
+            const CustomCircularContainer(
+                aim: 'Energy',
+                imagePath: 'assets/images/battery.jpg',
+                secondAim: 'performance',
+                secondImagePath: 'assets/images/Performance.jpg')
           ],
         ));
+  }
+}
+
+class CustomCircularContainer extends StatelessWidget {
+  final String imagePath;
+  final String aim;
+  final String secondImagePath;
+  final String secondAim;
+  const CustomCircularContainer({
+    required this.aim,
+    required this.imagePath,
+    required this.secondAim,
+    required this.secondImagePath,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color: Colors.white),
+      margin: const EdgeInsets.only(left: 20, right: 20),
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                 
+                },
+                child: Image.asset(
+                  imagePath,
+                  height: 60,
+                  width: 60,
+                ),
+              ),
+              Text(aim),
+            ],
+          ),
+          Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  
+                },
+                child: Image.asset(
+                  secondImagePath,
+                  height: 60,
+                  width: 60,
+                ),
+              ),
+              Text(secondAim),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
 
