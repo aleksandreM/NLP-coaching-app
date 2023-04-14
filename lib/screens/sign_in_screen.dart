361a837601ee209122e5856b5ad0ab966ed32f14
aleksandre murghulia <alexandre.murgulia@gmail.com>
2023-04-15 01:14:40 +0400

    ongoing work on bottom navigation bar in sign in page

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -64,7 +64,40 @@ class SignInScreen extends StatelessWidget {
                 secondAim: 'performance',
                 secondImagePath: 'assets/images/Performance.jpg')
           ],
-        ));
+        ),
+        bottomNavigationBar: ClipRRect(
+          borderRadius: BorderRadius.circular(50),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            currentIndex: 0,
+            items: [
+            BottomNavigationBarItem(icon: Image.asset(
+        'assets/images/Home.jpg',
+        height: 50,
+        width: 50,
+      ), label: ''),
+      BottomNavigationBarItem(icon: Image.asset(
+        'assets/images/Lamp.jpg',
+        height: 50,
+        width: 50,
+      ), label: ''),
+      BottomNavigationBarItem(icon: Image.asset(
+        'assets/images/Balance.jpg',
+        height: 50,
+        width: 50,
+      ), label: ''),
+      BottomNavigationBarItem(icon: Image.asset(
+        'assets/images/Companies.jpg',
+        height: 50,
+        width: 50,
+      ), label: ''),
+      BottomNavigationBarItem(icon: Icon(Icons.abc), label: ''),
+
+          
+           
+          ]),
+        ),
+        );
   }
 }
 
