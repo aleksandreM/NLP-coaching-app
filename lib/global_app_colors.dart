0496591f84ffa8541cf0e4b4b67bcdd49630fa85
aleksandre murghulia <alexandre.murgulia@gmail.com>
2023-04-26 12:21:35 +0400

    some more work on objectives screen

--- lib/global_app_colors.dart
+++ lib/global_app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class GlobalAppColors {
-  static const Color mainBackgroundColor = Color(0xFFF5F5F5);
+  static const Color mainBackgroundColor = Color.fromARGB(255, 234, 234, 234);
   static const Color buttonSignInTextCollor = Colors.black;
   static const Color buttonSignInBackgroundCollor = Color.fromRGBO(193, 179, 108, 1);
   static const Color buttonSignOutTextCollor = Color.fromRGBO(193, 179, 108, 1);
